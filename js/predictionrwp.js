ea54a3b22c081a3ea3458ceee7fa0a9eb577e8b7
wikish786 <88264704+wikish786@users.noreply.github.com>
2022-05-15 17:15:30 +0100

    Update predictionrwp.js

--- js/predictionrwp.js
+++ js/predictionrwp.js
@@ -8,8 +8,7 @@ var myChart = new Chart(ctx, {
   // The data for our dataset
   data: {
     // Data Labels
-    labels: ["prediction","crime today","No crime today","crime tomorrow","no crime tomorrow"],
-
+    labels: ["prediction of crime Tomorrow","YES","NO"],
     datasets: [
       // Data Set 1
       {
@@ -17,7 +16,7 @@ var myChart = new Chart(ctx, {
         label: "Crime",
 
         // Actual Data
-        data: [0,30,70, 25,75],
+        data: [0,30,70],
 
         // Background Color
         backgroundColor: [
